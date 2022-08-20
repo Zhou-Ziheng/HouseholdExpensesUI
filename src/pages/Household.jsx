f9ac7d2fbac2f0fdc9339051548cbd4a7a381726
SparkyFnay <z1362110538@gmail.com>
2022-08-19 21:50:30 -0400

    added mock data and basic Household component

---
+++ src/pages/Household.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { makeHousehold } from "../MockData/MockData";
+import { fetchHouseholdById } from "../MockData/utils/FetchData";
+
+const Household = ({ id }) => {
+  const data = fetchHouseholdById();
+
+  return (
+    <div>
+      <h1>Household Name: {data.name}</h1>
+      <h2>Total Allowance: {data.totalAllowance}</h2>
+      <h2>Number of Members: {data.members.length}</h2>
+
+      {data.members.map((member) => (
+        <>
+          <h3>
+            name: {member.name} | Allowance: {member.allowance}
+          </h3>
+          {member.expenses.map(({ category, items }) => (
+            <>
+              <h4>{category}: </h4>
+              {items.map((item) => (
+                <h5>
+                  title: {item.title} | Descrption: {item.description} | Amount:{" "}
+                  {item.amount}
+                </h5>
+              ))}
+            </>
+          ))}
+          <p>
+            ________________________________________________________________________________________________________________
+          </p>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default Household;
