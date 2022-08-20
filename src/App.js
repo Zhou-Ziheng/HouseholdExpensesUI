a8a1315d7d00829228df819cca2f4a3c0b769e68
Saad Naeem <naeemsaad1035@gmail.com>
2022-08-20 00:59:26 -0400

    Created Sign In Screen

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Household from "./pages/Household";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Signup from "./components/Signup";
+import Signup from "./components/Signup/Signup";
+import Home from "./components/Home/Home";
+import Signin from "./components/Signin/Signin";
 
 function App() {
   return (
@@ -10,8 +12,11 @@ function App() {
       <Navbar />
       <Router>
         <Routes>
-          <Route exact path="/" element={<Household />} />
+          {/* <Route exact path="/" element={<Household />} /> */}
+          <Route exact path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
         </Routes>
       </Router>
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
-import "./Navbar.css";
+import React from 'react'
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <div className="Navbar">
-      <span className="nav-logo">Insert Logo Here</span>
+      <a className="nav-logo" href="/">Insert Logo Here</a>
       <div className="nav-items">
-        <a href="/home">Home</a>
-        <a href="/Signup">Sign Up</a>
-        <a className="bg-blue-500 py-2 px-4 rounded opacity-100" href="/signin">
+        <a className="font-bold" href="/signup">Sign Up</a>
+        <a className="font-bold bg-[#4066E0] py-4 px-6 rounded opacity-100" href="/signin">
           Sign In
         </a>
       </div>
