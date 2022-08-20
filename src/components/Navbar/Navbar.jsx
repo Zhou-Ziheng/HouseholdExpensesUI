26282ea171d86edbda117f32146e9ff7e41a8925
Rayane Tayache <slamaper1@gmail.com>
2022-08-20 14:18:46 +0200

    4 logos added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import logo from "../../image/logo1.svg"
 import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <div className="Navbar">
-      <a className="nav-logo" href="/">Insert Logo Here</a>
+      <a className="nav-logo" href="/">
+        <img src={logo} alt="logo" height="300" width="180"/>
+      </a>
       <div className="nav-items">
         <a className="font-bold" href="/signup">Sign Up</a>
         <a className="font-bold bg-[#4066E0] py-4 px-6 rounded opacity-100" href="/signin">
