32c89a76918c12d1309c7faad21a124e939dee0c
Rayane Tayache <slamaper1@gmail.com>
2022-08-20 20:47:10 +0200

    addexpenses page + Navbardashboard updated

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./components/Signup/Signup";
 import Home from "./components/Home/Home";
 import Signin from "./components/Signin/Signin";
+import AddExpenses from "./pages/AddExpenses";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          {/* <Route exact path="/" element={<Household />} /> */}
           <Route exact path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/Home" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/household" element={<Household />} />
+          <Route path="/addexpenses" element={<AddExpenses />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/AddExpenses.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import NavbarDashboard from './NavbarDashboard'
+
+export default function AddExpenses() {
+    return (
+        <>
+            <NavbarDashboard />
+            <div className='h-[800px] bg-gray-50 flex flex-col justify-center'>
+                <div className='max-w-md w-full mx-auto'>
+                    <div className='text-center font-extrabold text-4xl py-2'> Add Your Daily Expenses </div>
+                </div>
+                <div className='max-w-md w-full mx-auto mt-4 bg-white p-8 border border-gray-300'>
+                    <form action="" className='space-y-6'>
+                        <div>
+                            <label htmlFor="" className='text-sm text-left font-bold text-gray-600 block'>Amount</label>
+                            <input type="number" className='w-full p-2 border border-gray-300 rounded mt-1'></input>
+                        </div>
+                        <div>
+                            <label htmlFor='' className='text-sm text-left font-bold text-gray-600 block'>Category</label>
+                            <select className='w-full p-2 border border-gray-300 rounded mt-1 '>
+                                <option value="Housing">Housing</option>
+                                <option value="Education">Education</option>
+                                <option value="Groceries">Groceries</option>
+                            </select>
+                        </div>
+                        <div>
+                            <button className='w-full py-2 px-4 bg-[#8b74bd] hover:bg-[#7953ab] rounded-md text-white text-sm'>Add</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
