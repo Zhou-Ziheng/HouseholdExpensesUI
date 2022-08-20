f8713346b08506902cd7ae361b00a0d6508331b2
Rayane Tayache <slamaper1@gmail.com>
2022-08-20 21:03:39 +0200

    add expenses

--- src/pages/AddExpenses.jsx
+++ src/pages/AddExpenses.jsx
@@ -8,6 +8,7 @@ export default function AddExpenses() {
             <div className='h-[800px] bg-gray-50 flex flex-col justify-center'>
                 <div className='max-w-md w-full mx-auto'>
                     <div className='text-center font-extrabold text-4xl py-2'> Add Your Daily Expenses </div>
+                    <div className='text-l font-medium text-gray-900 mt-2 text-center'> Updating your expenses to make sure you get the latest version of your family household</div>
                 </div>
                 <div className='max-w-md w-full mx-auto mt-4 bg-white p-8 border border-gray-300'>
                     <form action="" className='space-y-6'>
