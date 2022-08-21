fc5570769e9ab6afd8716505ee171ed91d0b4435
SparkyFnay <z1362110538@gmail.com>
2022-08-21 02:14:45 -0400

    asdf

--- src/components/PsDashboard/PsDash.jsx
+++ src/components/PsDashboard/PsDash.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router";
 import getMemberData from "../../APIs/getMemberData";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const headers = [
   "Users",
@@ -19,6 +20,10 @@ const headers = [
 
 export default function PsDash({ loggedin, id }) {
   const [perData, setPerData] = useState();
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const navigate = useNavigate();
   useEffect(() => {
@@ -62,21 +67,68 @@ export default function PsDash({ loggedin, id }) {
 
   return (
     <>
-      <h2 className="text-3xl mx-[200px] my-[40px]  font-bold  text-[#7953a9] ">
-        Personal Account Information Of
-      </h2>
-      <h1 className="mx-[200px] my-[-35px] text-5xl font-bold">
-        {perData?.name}
-      </h1>
-      <h3 className="text-2xl font-light mx-[200px] my-[45px]">
-        ${perData?.used} used out of ${perData?.allowance}
-      </h3>
+      {" "}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Expenses</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="ControlInput1">
+              <Form.Label>Amount</Form.Label>
+            </Form.Group>
+            <Form.Control type="text" placeholder="Insert Amount" autoFocus />
+            <Form.Group className="mb-3" controlId="ControlInput2">
+              <Form.Label>Category</Form.Label>
+              <Form.Select aria-label="Select" placeholder="Select A Category">
+                <option value="Utilities">Utilities</option>
+                <option value="School">School</option>
+                <option value="Groceries">Groceries</option>
+                <option value="Rent">Rent</option>
+                <option value="Clothes">Clothes</option>
+                <option value="Subscriptions">Subscriptions</option>
+                <option value="Entertainment">Entertainment</option>
+                <option value="Miscellaneous">Miscellaneous</option>
+              </Form.Select>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <button variant="secondary" onClick={handleClose}>
+            Close
+          </button>
+          <button variant="primary" onClick={handleClose}>
+            Add Expenses
+          </button>
+        </Modal.Footer>
+      </Modal>
+      <div className="flex flex-col-2">
+        <div>
+          <h2 className="text-3xl mx-[200px] my-[40px]  font-bold  text-[#7953a9] ">
+            Personal Account Information Of
+          </h2>
+          <h1 className="mx-[200px] my-[-35px] text-5xl font-bold">
+            {perData?.name}
+          </h1>
+          <h3 className="text-2xl font-light mx-[200px] my-[45px]">
+            ${perData?.used} used out of ${perData?.allowance}
+          </h3>
+        </div>
+        <div className="mx-auto my-auto">
+          <button
+            onClick={handleShow}
+            className="w-full py-7 px-7 bg-[#8b74bd] hover:bg-[#7953ab] rounded-md text-white font-semibold text-m whitespace-nowrap transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+          >
+            Add Expenses
+          </button>
+        </div>
+      </div>
       <div class="my-[-25px] mx-[-175px] rounded relative sm:rounded-lg">
         <table class="w-4/6 text-sm mx-auto text-left rounded text-gray-400">
           <caption class="p-5 text-lg font-semibold rounded text-left text-white bg-[#8b74bd]">
             Expenses
             <p class="mt-1 text-sm font-normal text-gray-200">
-              Table of MemberFirst's expenses sorted through categories
+              Table of {perData?.name}'s expenses sorted through categories
               (Utilities, School, Groceries, Rent, Clothes, Subscriptions,
               Entertainment, Miscellaneous) and amount of each category.
             </p>
