de97f8605376b9f5b2e8c61644fdd18f4a512a41
SparkyFnay <z1362110538@gmail.com>
2022-08-21 01:24:47 -0400

    styles

--- src/components/PsDashboard/PsDash.jsx
+++ src/components/PsDashboard/PsDash.jsx
@@ -43,16 +43,16 @@ export default function PsDash({ loggedin, id }) {
         ${perData?.used} used out of ${perData?.allowance}
       </h3>
       <div class="my-[-25px] mx-[-175px] rounded relative sm:rounded-lg">
-        <table class="w-4/6 text-sm mx-auto text-left text-gray-500 rounded dark:text-gray-400">
-          <caption class="p-5 text-lg font-semibold rounded text-left text-gray-900 bg-white dark:text-white dark:bg-[#8b74bd]">
+        <table class="w-4/6 text-sm mx-auto text-left rounded text-gray-400">
+          <caption class="p-5 text-lg font-semibold rounded text-left text-white bg-[#8b74bd]">
             Expenses
-            <p class="mt-1 text-sm font-normal text-gray-200 dark:text-gray-200">
+            <p class="mt-1 text-sm font-normal text-gray-200">
               Table of MemberFirst's expenses sorted through categories
               (Utilities, School, Groceries, Rent, Clothes, Subscriptions,
               Entertainment, Miscellaneous) and amount of each category.
             </p>
           </caption>
-          <thead class="rounded text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <thead class="rounded text-xs uppercase bg-gray-700 text-gray-400">
             <tr>
               <th scope="col" class="py-3 px-6">
                 Category
@@ -66,19 +66,16 @@ export default function PsDash({ loggedin, id }) {
             </tr>
           </thead>
           <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr class="bg-gray-800 border-gray-700">
               <th
                 scope="row"
-                class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                class="py-4 px-6 font-medium whitespace-nowrap text-white"
               >
                 Utilities
               </th>
               <td class="py-4 px-6">$2999</td>
               <td class="py-4 px-6 text-right">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
+                <a href="#" class="font-medium text-blue-500 hover:underline">
                   Edit
                 </a>
               </td>
