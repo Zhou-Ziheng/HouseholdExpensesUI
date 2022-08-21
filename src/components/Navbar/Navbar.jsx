bc8343443e050b9d65d530a7f9e624b5a8aaf712
Rayane Tayache <slamaper1@gmail.com>
2022-08-21 13:39:56 +0200

    adjust nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = ({ loggedin, setLogin }) => {
       <div class="Shadow">
         <div className="Navbar">
           <a className="nav-logo" href="/">
-            <img src={logo} alt="logo" height="300" width="180" />
+            <img src={logo} alt="logo" height="300" width="125" />
           </a>
           <div className="nav-items">
             <a className="font-bold" href="/help">
