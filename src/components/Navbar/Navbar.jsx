dad9fd10e9cc040bd738a168094d584d56d7614f
Saad Naeem <naeemsaad1035@gmail.com>
2022-08-20 23:21:25 -0400

    added help button to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,6 +14,9 @@ const Navbar = ({ loggedin, setLogin }) => {
             <img src={logo} alt="logo" height="300" width="180" />
           </a>
           <div className="nav-items">
+            <a className="font-bold" href="/help">
+              Help
+            </a>
             <a className="font-bold" href="/signup">
               Sign Up
             </a>
